8797da0af4486087c25ca0667675df23850dedfe
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-08-18 16:26:39 -0500

    import css

--- react portfolio/src/main.jsx
+++ react portfolio/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/css/bootstrap.min.css';
+import './assets/css/animate.css';
+import './assets/css/LineIcons.2.0.css';
+import './assets/css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
