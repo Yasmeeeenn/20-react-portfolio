e0fc76a2b2bf3492214393412dc91152a99c01ab
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-08-18 19:38:49 -0500

    added about me

---
+++ react portfolio/src/your_info.jsx
@@ -0,0 +1,161 @@
+const backgroundImageUrl = 'https://unsplash.com/photos/xrVDYZRGdw4';
+
+
+
+const name = {
+  firstname: 'Yasmeen',
+  lastname: 'Nguyen',
+
+  url: 'https://www.google.com',
+};
+
+
+const typeWriterText = [
+  'Software Engineer',
+  'Aerial Artist',
+    
+];
+
+
+const socialProfiles = [
+  {
+    name: 'LinkedIn',
+    icon: 'lni lni-linkedin',
+    url: '',
+  },
+  {
+    name: 'GitHub',
+    icon: 'lni lni-github',
+    url: '',
+  },
+
+];
+
+
+
+
+const categories = [
+  {
+    name: 'Web Development',
+    icon: 'lni lni-code',
+    title: 'Photography',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-camera',
+        title: 'HTML5',
+      },
+      {
+        icon: 'lni lni-certificate',
+        title: 'CSS3',
+      },
+      {
+        icon: 'lni lni-instagram',
+        title: 'JavaScript',
+      },
+    ],
+  },
+  {
+    name: 'Software',
+    icon: 'lni lni-code',
+    title: 'Software',
+    description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-adobe',
+        title: 'adobe',
+      },
+      {
+        icon: 'lni lni-code',
+        title: 'Code',
+      },
+      {
+        icon: 'lni lni-sketch',
+        title: 'Sketch'
+      },
+     
+      
+    ],
+  },
+  {
+    name: 'brands',
+    icon: 'lni lni-code',
+    title: 'Brands',
+    description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-pinterest',
+        title: 'Pinterest',
+      },
+      {
+        icon: 'lni lni-crop',
+        title: 'Crop',
+      },
+      
+    ],
+  },
+  
+];
+
+
+const achievements = [
+  { word: 'random', value: 100, unit: '%' },
+  { word: 'finished projects', value: 7500, unit: '' },
+  { word: 'experience', value: 20, unit: ' years' },
+];
+
+
+
+
+const projectData = [
+  {
+    title: 'BEZY',
+    description: 'Exercise index',
+    demoUrl: 'https://github.com/Yasmeeeenn/BEZY' 
+  },
+  {
+    title: 'Work Day Planner',
+    description: 'simple calender application',
+    demoUrl: 'https://github.com/Yasmeeeenn/5-work-day-planner' 
+  },
+  {
+    title: 'ReadMe Generator',
+    description: 'produces a readme for your application',
+    demoUrl: 'https://github.com/Yasmeeeenn/9-README-Generator' 
+  },
+  {
+    title: 'Password Generator',
+    description: '',
+    demoUrl: 'https://github.com/Yasmeeeenn/Password-Generator-0.0' 
+  },
+  
+];
+
+
+//////////////////////////////////////// ** FOOTER SECTION **//////////////////////////////////////////////////
+
+
+
+
+const aboutMeText = {
+  infotext: `Hi there, I'm Yasmeen.`,
+  power_slogan: `Making ideas fly!`,
+};
+
+
+
+
+export {
+  backgroundImageUrl,
+  name,
+  typeWriterText,
+  socialProfiles,
+  categories,
+  achievements,
+  projectData,
+  aboutMeText,
+  
+};
